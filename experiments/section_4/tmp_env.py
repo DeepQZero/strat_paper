c02b5fc82b04dcf473485e19452e42d1c7a39f5c
sood0027 <sood0027@umn.edu>
2023-08-05 10:09:18 -0500

    Section 4 Files
    Section 4 files

---
+++ experiments/section_4/tmp_env.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+from lib import dynamics as dyn
+
+
+class Env:  # TODO RENAME SpaceEnv
+    def __init__(self, step_len: int = 10800, dis: int = 180,
+                 max_turns: int = 8*14, max_fuel=200) -> None:  # TODO Change
+        self.DIS = dis
+        self.UP_LEN = step_len / dis
+        self.MAX_TURNS = max_turns
+        self.MAX_FUEL = max_fuel
+        self.mobile = None
+        self.cap_base = None
+        self.ret_base = None
+        self.time_step = None
+        self.total_fuel = None
+
+    def reset(self) -> np.ndarray:
+        """Resets environment and returns observation per Gym Standard."""
+        self.mobile = np.array([-dyn.GEO, 0.0, 0.0, -dyn.BASE_VEL_Y])
+        self.ret_base = np.array([-dyn.GEO, 0.0, 0.0, -dyn.BASE_VEL_Y])
+        self.cap_base = np.array([dyn.GEO, 0.0, 0.0, dyn.BASE_VEL_Y])
+        self.time_step = 0
+        self.total_fuel = 0
+        return self.det_obs()
+
+    def det_reset(self, mobile, ret_base, cap_base, time_step, total_fuel) -> np.ndarray:
+        """Resets environment deterministically."""
+        self.mobile = mobile
+        self.ret_base = ret_base
+        self.cap_base = cap_base
+        self.time_step = time_step
+        self.total_fuel = total_fuel
+        return self.det_obs()
+
+    def det_reset_helper(self, state) -> np.ndarray:
+        """Resets environment deterministically."""
+        self.mobile = state[0:4]
+        self.ret_base = state[4:8]
+        self.cap_base = state[8:12]
+        self.time_step = state[12]
+        self.total_fuel = state[13]
+        return self.det_obs()
+
+    def det_obs(self) -> np.ndarray:
+        """Returns observation per Gym standard."""
+        return np.concatenate((self.mobile, self.ret_base,
+                               self.cap_base, [self.time_step], [self.total_fuel]))
+
+    def step(self, action: np.ndarray) -> tuple:
+        """Advances environment forward one time step, returns Gym signals."""
+        action = np.array([0.0, 0.0]) if self.total_fuel > self.MAX_FUEL else action
+        self.mobile[2:4] += action
+        self.total_fuel += dyn.vec_norm(action)
+        self.mobile = self.prop_unit(self.mobile)
+        self.ret_base = self.prop_unit(self.ret_base)
+        self.cap_base = self.prop_unit(self.cap_base)
+        self.time_step += 1
+        return self.det_obs(), self.det_reward(), self.is_done(), {}
+
+    def is_done(self) -> bool:
+        """Determines if episode has reached termination."""
+        return self.is_capture() or self.is_timeout()
+
+    def is_capture(self) -> bool:
+        return dyn.vec_norm(self.mobile[0:2] - self.cap_base[0:2]) < 5e5
+
+    def is_timeout(self) -> bool:
+        return self.time_step == self.MAX_TURNS
+
+    def det_reward(self) -> float:
+        """Returns reward at current time step."""
+        if self.is_done():
+            return 0.0
+        else:
+            return 0.0
+
+    def prop_unit(self, unit: np.ndarray) -> np.ndarray:
+        """Propagates a given unit state forward one time step."""
+        return dyn.propagate(unit[0:4], self.DIS, self.UP_LEN)
+
+
+def env_test():
+    env = Env()
+    for i in range(100):
+        print(i)
+        _ = env.reset()
+        done = False
+        while not done:
+            state, reward, done, info = env.step(np.random.randint(9))
+
+
+# if __name__ == "__main__":
+#     env_test()
---
+++ experiments/section_4/tmp_test_exp.py
@@ -0,0 +1,64 @@
+from multiprocessing import Pool
+import time
+import pickle
+
+import numpy as np
+
+from lib import dynamics as dyn
+from tmp_env import Env
+
+
+def one_episode(i):
+    """Gets data from one episode."""
+    MAX_FUEL = 200
+    MAX_TURNS = 112
+    thrust = 10
+    np.random.seed()
+    env = Env()
+    state = env.reset()
+    done = False
+    while not done:
+        rand_thrust = np.random.uniform(0, thrust)
+        rand_angle = np.random.uniform(0, 2 * np.pi)
+        rand_act = np.array([np.cos(rand_angle), np.sin(rand_angle)]) * rand_thrust
+        # if np.random.uniform(0, 1) < 0.75:
+        #     rand_act = np.array([0.0, 0.0])
+        if ((MAX_FUEL - state[13]) / MAX_FUEL) < 0.5 and (dyn.abs_angle_diff(state[0:2], state[8:10]) > (np.pi/2)):
+            rand_act = np.array([0.0, 0.0])
+        if ((MAX_FUEL - state[13]) / MAX_FUEL) < 0.25 and (dyn.abs_angle_diff(state[0:2], state[8:10]) > (np.pi/4)):
+            rand_act = np.array([0.0, 0.0])
+        if ((MAX_FUEL - state[13]) / MAX_FUEL) < 0.125 and (dyn.abs_angle_diff(state[0:2], state[8:10]) > (np.pi/8)):
+            rand_act = np.array([0.0, 0.0])
+        state, reward, done, info = env.step(rand_act)
+
+        angle = dyn.abs_angle_diff(state[0:2], state[8:10])
+        dist = dyn.vec_norm(state[0:2])
+        in_zone = abs(dist - dyn.GEO) < 5e6
+        angle_left_proportion = angle / np.pi
+        fuel_left_proportion = (MAX_FUEL*1 - state[13]) / (MAX_FUEL*1)
+        turn_left_proportion = (MAX_TURNS - state[12]) / MAX_TURNS
+        good_fuel = angle_left_proportion < fuel_left_proportion
+        good_turn = angle_left_proportion < turn_left_proportion
+        if in_zone and good_fuel and good_turn and abs(dyn.vec_norm(state[0:2]) - dyn.GEO) < 1e7:
+            print(angle_left_proportion, "{:e}".format(abs(dyn.vec_norm(state[0:2]) - dyn.GEO)),
+                  "{:e}".format(dyn.vec_norm(state[0:2] - state[8:10])), fuel_left_proportion, turn_left_proportion)
+            print('SUCCESS', i)
+            # done = True
+        if dyn.vec_norm(state[0:2] - state[8:10]) < 5e5:
+            print('CAPTURE ', i, fuel_left_proportion)
+            done = True
+    return None
+
+
+def main_exp():
+    """Gets experiment data and returns dictionary of polished data."""
+    # get experiment raw data
+    tic = time.time()
+    with Pool(16) as p:
+        _ = p.map(one_episode, list(range(int(1e6))))
+    toc = time.time()
+    print(tic-toc)
+
+
+if __name__ == "__main__":
+    main_exp()
