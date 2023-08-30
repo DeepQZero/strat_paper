319ee4956b0d00bd99e9f10d34a4af57efd19b8b
sood0027 <sood0027@umn.edu>
2023-08-30 17:34:41 -0400

    2.6 and 2.7 update

--- section_2/exp_2_6_atari_learning/exp_2_6_plots.py
+++ section_2/exp_2_6_atari_learning/exp_2_6_plots.py
@@ -14,69 +14,36 @@ def proc_results(result_hist):
     for result in result_hist:
         timestep = result[0]
         adj_result = ((np.array(result[1:]) - 0.01) / 0.96)[0]
-        passive_prob = adj_result[0] + adj_result[1]
+        passive_prob = adj_result[0]# + adj_result[1]
+        no_op_prob = adj_result[1]
         right_prob = adj_result[2]
         left_prob = adj_result[3]
         #adj_result = np.array([passive_prob, right_prob, left_prob])
-        adj_result = {"NO OP": passive_prob, "RIGHT": right_prob, "LEFT":left_prob}
-        # Combine passive action and fire
-        #new_result_hist.append(adj_result)
-        new_result_hist[str(timestep) + " steps"] = adj_result
-    return new_result_hist
-
-# Subtract out the random exploration in the results.
-def proc_results2(result_hist):
-    new_result_hist = dict()
-    for result in result_hist:
-        timestep = result[0]
-        adj_result = ((np.array(result[1:]) - 0.01) / 0.96)[0]
-        passive_prob = adj_result[0] + adj_result[1]
-        right_prob = adj_result[2]
-        left_prob = adj_result[3]
-        #adj_result = np.array([passive_prob, right_prob, left_prob])
-        adj_result = {"NO OP": passive_prob, "RIGHT": right_prob, "LEFT":left_prob}
+        adj_result = {"PASSIVE": passive_prob, "FIRE": no_op_prob, "RIGHT": right_prob, "LEFT":left_prob}
         # Combine passive action and fire
         #new_result_hist.append(adj_result)
         new_result_hist[timestep] = adj_result
     return new_result_hist
 
-def lineplot():
-    result_hist = pickle.load(open("exp_2_6_data.pkl", "rb"))
-    new_result_hist = proc_results(result_hist)
-    steps_to_plot = [10000, 20000, 50000, 100000]
-    result_df = dict()
-    for s in steps_to_plot:
-        key = str(s)+" steps"
-        action_dist = new_result_hist[key]
-        result_df[key] = action_dist
-    result_df = pd.DataFrame(result_df)
-    ax = sns.lineplot(result_df, palette=palette)
-    line = ax.get_lines()
-    #plt.grid(alpha=0.25)
-    ax.fill_between(line[0].get_xdata(), line[1].get_ydata(), color=palette[0], alpha=0.8)
-    ax.fill_between(line[1].get_xdata(), line[2].get_ydata(), color=palette[1], alpha=0.8)
-    ax.fill_between(line[2].get_xdata(), line[3].get_ydata(), color=palette[2], alpha=0.8)
-    ax.set(xlabel="Action", ylabel="Probability", title="Agent Succumbs to Sparsity in ATARI Breakout")
-    plt.savefig("2_6_fig.png")
-    plt.show()
-    return
-
 def stacked_density_plot():
     result_hist = pickle.load(open("exp_2_6_data.pkl", "rb"))
-    new_result_hist = proc_results2(result_hist)
-    no_op = dict()
+    new_result_hist = proc_results(result_hist)
+    passive = dict()
+    fire = dict()
     right = dict()
     left = dict()
     for key in new_result_hist.keys():
-        no_op[key] = new_result_hist[key]["NO OP"] + new_result_hist[key]["RIGHT"] + new_result_hist[key]["LEFT"]
+        passive[key] = new_result_hist[key]["PASSIVE"] + new_result_hist[key]["FIRE"] + new_result_hist[key]["RIGHT"] + new_result_hist[key]["LEFT"]
+        fire[key] = new_result_hist[key]["FIRE"] + new_result_hist[key]["RIGHT"] + new_result_hist[key]["LEFT"]
         right[key] = new_result_hist[key]["RIGHT"] + new_result_hist[key]["LEFT"]
         left[key] = new_result_hist[key]["LEFT"]
-    result_df = pd.DataFrame({"NO OP":no_op, "RIGHT": right, "LEFT": left})
-    ax = sns.lineplot(result_df, palette=palette, linewidth=4, dashes=False)
+    result_df = pd.DataFrame({"PASSIVE":passive, "FIRE":fire, "RIGHT": right, "LEFT": left})
+    ax = sns.lineplot(result_df, palette=palette, linewidth=2, dashes=False)
     line = ax.get_lines()
-    ax.fill_between(line[0].get_xdata(), line[0].get_ydata(), color=palette[0], alpha=0.6)
-    ax.fill_between(line[0].get_xdata(), line[1].get_ydata(), color=palette[1], alpha=0.6)
-    ax.fill_between(line[0].get_xdata(), line[2].get_ydata(), color=palette[2], alpha=0.6)
+    ax.fill_between(line[0].get_xdata(), line[0].get_ydata(), color=palette[0], alpha=1.0)
+    ax.fill_between(line[0].get_xdata(), line[1].get_ydata(), color=palette[1], alpha=1.0)
+    ax.fill_between(line[0].get_xdata(), line[2].get_ydata(), color=palette[2], alpha=1.0)
+    ax.fill_between(line[0].get_xdata(), line[3].get_ydata(), color=palette[3], alpha=1.0)
     ax.set(xlabel="Timestep", ylabel="Action Density", title="Agent Succumbs to Sparsity in ATARI Breakout")
     plt.savefig("2_6_fig.png")
     plt.show()
